aacdc2c9adbbcbbbebe7b01fe7ed23185e567028
doloi01 <mauvatuyet@gmail.com>
2022-02-22 08:37:42 +0700

    fix loi~ khi ctrl f5 k load js o department.index

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,6 +30,33 @@
 
     </div>
     <script type="text/javascript" src="{{ mix('/js/app.js') }}"></script>
-  
+    <!-- jQuery -->
+<script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- DataTables  & Plugins -->
+  <script src="{{ asset('admin/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ asset('admin/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('admin/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+  <script src="{{ asset('admin/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('admin/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+  <script src="{{ asset('admin/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('admin/plugins/jszip/jszip.min.js') }}"></script>
+  <script src="{{ asset('admin/plugins/pdfmake/pdfmake.min.js') }}"></script>
+  <script src="{{ asset('admin/plugins/pdfmake/vfs_fonts.js') }}"></script>
+  <script src="{{ asset('admin/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+  <script src="{{ asset('admin/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+ 
+  <!-- Page specific script -->
+  <script>
+    $(function () {
+      $("#example1").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["copy", "excel", "pdf", "print"],
+        "bDestroy": true,
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      
+    });
+  </script>
 </body>
 </html>
